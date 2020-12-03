8a1bd7bc0f98453a3fcabfd1d5287a7b56db3462
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2020-12-03 08:33:23 -0500

    case in environment.js switched to heroku

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'https://sperkins28.github.io/Ratings-Client/':
+    case 'https://reviewit-client.herokuapp.com/':
         APIURL = 'https://re-view-it.herokuapp.com'
 }
 
